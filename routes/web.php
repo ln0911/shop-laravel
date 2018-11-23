721f52be7ec585d1ffc0800fdced11775fa2ec27
vagrant <vagrant@homestead>
2018-11-23 15:54:14 +0000

    触发激活邮件

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,12 @@ Auth::routes();
 
 Route::group(['middleware'=>'auth'],function(){
     Route::get('/email_verify_notice','PagesController@emailVerifyNotice')->name('email_verify_notice');
+    Route::get('/email_verification/verify','EmailVerificationController@verify')->name('email_verification.verify');
+    Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
 
     //邮箱验证开始
      Route::group(['middleware'=>'email_verified'],function (){
-        Route::get('/test',function (){
-            return 'this is test email verified';
-        }) ;
+
      });
     //结束
 });
