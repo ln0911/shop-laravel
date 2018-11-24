912a66cc42aad1521eaa77aa7664c1ed913cdca1
vagrant <vagrant@homestead>
2018-11-24 12:47:34 +0000

    管理后台 - 商品列表

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = [
-        'title','description','image','on_sale','rating','sold_count','review_count','price';
+        'title','description','image','on_sale','rating','sold_count','review_count','price'
     ];
     protected $casts = [
         'on_sale' => 'boolean'
