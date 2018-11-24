8cf6701c54bcf7e13d0c6e25965a83fe3c89efcb
vagrant <vagrant@homestead>
2018-11-24 17:52:45 +0000

    购物车列表

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,10 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\AddCartRequest;
 use App\Models\CartItem;
+use App\Models\ProductSku;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
+
+    /**
+     * 购物车列表
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+        $cartItems = $request->user()->cartItems()->with('productSku.product')->get();
+        return view('cart.index',['cartItems'=>$cartItems]);
+    }
+    /**
+     * add cart
+     * @param AddCartRequest $request
+     * @return array
+     */
     public function add(AddCartRequest $request)
     {
         $user    = $request->user();
@@ -23,6 +40,18 @@ class CartController extends Controller
             $cart->productSku()->associate($skuId);
             $cart->save();
         }
+        return [];
+    }
+
+    /**
+     * 移除
+     * @param ProductSku $sku
+     * @param Request $request
+     * @return array
+     */
+    public function remove(ProductSku $sku , Request $request)
+    {
+        $request->user()->cartItems()->where('product_sku_id',$sku->id)->delete();
         return [];
     }
 }
