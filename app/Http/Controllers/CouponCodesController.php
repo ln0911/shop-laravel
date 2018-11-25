b9c21ff1fef5e9139a42b58a93428703e8a5e5bd
vagrant <vagrant@homestead>
2018-11-25 18:59:31 +0000

    优化优惠券模块

--- app/Http/Controllers/CouponCodesController.php
+++ app/Http/Controllers/CouponCodesController.php
@@ -10,13 +10,13 @@ class CouponCodesController extends Controller
 {
 
 
-    public function show($code)
+    public function show($code, Request $request)
     {
         // 判断优惠券是否存在
         if (!$record = CouponCode::where('code', $code)->first()) {
             abort(404);
         }
-        $record->checkAvailable();
+        $record->checkAvailable($request->user());
 
         return $record;
     }
