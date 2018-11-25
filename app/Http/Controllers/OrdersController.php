e1b18e606f8e08eb926293948c4d7141574dba0a
vagrant <vagrant@homestead>
2018-11-25 11:40:48 +0000

    用户界面-订单列表

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Exceptions\InvalidRequestException;
 use App\Http\Requests\OrderRequest;
+use App\Http\Requests\Request;
 use App\Jobs\CloseOrder;
 use App\Models\Order;
 use App\Models\ProductSku;
@@ -13,6 +14,24 @@ use Carbon\Carbon;
 class OrdersController extends Controller
 {
 
+    /**
+     * 订单列表
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index(Request $request){
+         $orders = Order::query()->with(['items.product','items.productSku'])
+                                 ->where('user_id',$request->user()->id)
+                                 ->orderBy('created_at','desc')
+                                 ->paginate();
+         return view('orders.index',['orders'=>$orders]);
+    }
+
+    /**
+     * 订单创建
+     * @param OrderRequest $request
+     * @return mixed
+     */
     public function store(OrderRequest $request)
     {
         $user  = $request->user();
--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,7 @@ Route::group(['middleware'=>'auth'],function(){
 
 
          Route::post('orders', 'OrdersController@store')->name('orders.store');
+         Route::get('orders', 'OrdersController@index')->name('orders.index');
 
 
      });
