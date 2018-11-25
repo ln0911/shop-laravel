78a1b76581e3e4e1c5f6f3b0a040f52776865e5a
vagrant <vagrant@homestead>
2018-11-25 12:12:12 +0000

    封面订单、购物车业务代码

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -5,10 +5,18 @@ namespace App\Http\Controllers;
 use App\Http\Requests\AddCartRequest;
 use App\Models\CartItem;
 use App\Models\ProductSku;
+use App\Services\CartService;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
+
+    protected $cartService;
+
+    public function __construct(CartService $cartService)
+    {
+        $this->cartService = $cartService;
+    }
 
     /**
      * 购物车列表
@@ -17,7 +25,7 @@ class CartController extends Controller
      */
     public function index(Request $request)
     {
-        $cartItems = $request->user()->cartItems()->with('productSku.product')->get();
+        $cartItems = $this->cartService->get();
         $addresses = $request->user()->addresses()->orderBy('last_used_at','desc')->get();
         return view('cart.index',['cartItems'=>$cartItems,'addresses'=>$addresses]);
     }
@@ -28,19 +36,9 @@ class CartController extends Controller
      */
     public function add(AddCartRequest $request)
     {
-        $user    = $request->user();
-        $skuId   = $request->input('sku_id');
-        $amount  = $request->input('amount');
 
-        if($cart = $user->cartItems()->where('product_sku_id',$skuId)->first()){
-            $cart->update(['amount'=>$cart->amount + $amount]);
-        }else{
+        $this->cartService->add($request->input('sku_id'),$request->input('amount'));
 
-            $cart  = new CartItem(['amount'=>$amount]);
-            $cart->user()->associate($user);
-            $cart->productSku()->associate($skuId);
-            $cart->save();
-        }
         return [];
     }
 
@@ -52,7 +50,7 @@ class CartController extends Controller
      */
     public function remove(ProductSku $sku , Request $request)
     {
-        $request->user()->cartItems()->where('product_sku_id',$sku->id)->delete();
+        $this->cartService->remove($sku->id);
         return [];
     }
 }
